a1ed7f47b1e8bd4f8980ac2c0c6a8ab2c460c62d
Nayan CM <nayan15-10316@diu.edu.bd>
2021-04-08 19:35:45 +0600

    true-false done

---
+++ true-false.js
@@ -0,0 +1,32 @@
+// True values
+const testVariable1 = true;
+const testVariable2 = "Bangladesh";
+const testVariable3 = " ";
+const testVariable4 = '0';
+const testVariable5 = -5;
+const testVariable6 = 5;
+
+// False values
+const testVariable7 = false;
+const testVariable8 = 0;
+const testVariable9 = "";
+const testVariable10 = undefined;
+const testVariable11 = null;
+const testVariable12 = NaN;
+
+
+const num = 5;
+if(num >= 0){
+    console.log("Condition is True.")
+}
+else{
+    console.log("Condition is False.")
+}
+
+const names = 0;
+if (names || names == 0) {
+    console.log("My condition is True.");
+}
+else{
+    console.log("My condition is False.");
+}
