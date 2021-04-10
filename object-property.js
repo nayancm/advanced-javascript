76875018933921be03b09fbd760a5dbdf51c477c
Nayan CM <nayan15-10316@diu.edu.bd>
2021-04-10 20:49:07 +0600

    Using map filter find get object properties

---
+++ object-property.js
@@ -0,0 +1,28 @@
+const students = [
+{id: 112, name: "Rahim"},
+{id: 113, name: "Karim"},
+{id: 114, name: "Jamal"},
+{id: 115, name: "Kamal"}
+];
+
+// using for loop
+/*
+const studentNames = [];
+for(let i = 0; i < students.length; i++){
+    const element = students[i];
+    studentNames.push(element.name);
+}
+console.log("Student names:", studentNames);*/
+
+// using map
+const studentName = students.map(student => student.name);
+console.log("Student Names:", studentName);
+
+const studentId = students.map(student => student.id);
+console.log("Student Ids:", studentId);
+
+const biggerId = students.filter(student => student.id > 113);
+console.log("Bigger ids:", biggerId);
+
+const biggerOne = students.find(student => student.id > 113);
+console.log("Bigger One:", biggerOne);
